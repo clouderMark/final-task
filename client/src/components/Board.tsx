807b6cdd3f3e997c3ae3c61c28bcde1eb2219ad7
clouderMark <charnysheuski@yandex.by>
2024-01-08 15:07:09 +0300

    feat: add theme to table

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -1,19 +1,34 @@
 import {TableContainer, Paper, Table, TableHead, TableRow, TableBody} from '@mui/material';
+import {useAppSelector} from '../redux/hooks';
+import {selectTheme} from '../styles/themeSlice/themeSlice';
+import {theme} from '../styles/theme';
 
 interface IProps {
   tableHeadCells?: JSX.Element;
   tableBodyCells: JSX.Element;
 }
 
-export const Board = (props: IProps) => (
-  <TableContainer component={Paper} sx={{mt: 2, mb: 2}}>
-    <Table size="small">
-      {props.tableHeadCells ? (
-        <TableHead>
-          <TableRow>{props.tableHeadCells}</TableRow>
-        </TableHead>
-      ) : null}
-      {props.tableBodyCells ? <TableBody>{props.tableBodyCells}</TableBody> : null}
-    </Table>
-  </TableContainer>
-);
+export const Board = (props: IProps) => {
+  const {type} = useAppSelector(selectTheme);
+
+  return (
+    <TableContainer
+      component={Paper}
+      sx={{
+        mt: 2,
+        mb: 2,
+        backgroundColor: theme.palette.first[type],
+        'th, td': {color: theme.palette.third[type]},
+      }}
+    >
+      <Table size="small">
+        {props.tableHeadCells ? (
+          <TableHead>
+            <TableRow>{props.tableHeadCells}</TableRow>
+          </TableHead>
+        ) : null}
+        {props.tableBodyCells ? <TableBody>{props.tableBodyCells}</TableBody> : null}
+      </Table>
+    </TableContainer>
+  );
+};
