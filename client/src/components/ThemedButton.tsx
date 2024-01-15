1221b69a91b87037781638fd35bc36effb4ea5a3
clouderMark <charnysheuski@yandex.by>
2024-01-15 16:54:54 +0300

    fix: prop component in button

--- client/src/components/ThemedButton.tsx
+++ client/src/components/ThemedButton.tsx
@@ -1,15 +1,29 @@
+import {ElementType} from 'react';
 import {Button, ButtonProps} from '@mui/material';
 import {useAppSelector} from '../redux/hooks';
 import {selectTheme} from '../styles/themeSlice/themeSlice';
 import {theme} from '../styles/theme';
 
-interface IProps extends ButtonProps {}
+interface IProps extends ButtonProps {
+  component?: ElementType;
+}
 
 const ThemedButton = (props: IProps) => {
   const {type} = useAppSelector(selectTheme);
 
   return (
-    <Button {...props} sx={{m: 1, color: theme.palette.third[type]}}>
+    <Button
+      {...props}
+      sx={{
+        color: theme.palette.third[type],
+        backgroundColor: theme.palette.first[type],
+        '&:hover': {
+          backgroundColor: theme.palette.second[type],
+        },
+        ...props.sx,
+      }}
+      component={props.component ? props.component : 'button'}
+    >
       {props.children}
     </Button>
   );
