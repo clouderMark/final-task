6a747702f6b0b7714e61fae3b90cc833c6489a74
clouderMark <charnysheuski@yandex.by>
2024-01-16 13:08:24 +0300

    feat: compoent for collection or smthng like this

---
+++ client/src/components/ListItem/ListItem.tsx
@@ -0,0 +1,32 @@
+import {useNavigate} from 'react-router-dom';
+import {Card, CardMedia, CardContent, Typography} from '@mui/material';
+import {card} from './styles';
+
+interface IProps {
+  navigateTo: string;
+  id: string | number;
+  image: string;
+  child: JSX.Element;
+}
+
+const ListItem = (props: IProps) => {
+  const {image, child, id, navigateTo} = props;
+  const navigate = useNavigate();
+
+  return (
+    <Card sx={card.card} onClick={() => navigate(`/${navigateTo}/${id}`)}>
+      {image ? (
+        <CardMedia sx={card.img} component="img" image={process.env.REACT_APP_IMG_URL + image} />
+      ) : (
+        <CardMedia sx={card.img} component="img" image="http://via.placeholder.com/335" />
+      )}
+      <CardContent sx={card.content}>
+        <Typography sx={card.title} component="strong">
+          {child}
+        </Typography>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default ListItem;
---
+++ client/src/components/ListItem/styles.ts
@@ -0,0 +1,47 @@
+import {theme} from '../../styles/theme';
+
+const tabletSize = 767;
+const mobileSize = 575;
+
+export const card = {
+  card: {
+    width: '31.66%',
+    marginBottom: '40px',
+    borderRadius: 0,
+    cursor: 'pointer',
+    '&:hover': {
+      color: theme.palette.first.main,
+    },
+    [`@media (max-width: ${tabletSize}px)`]: {
+      width: '48.5%',
+    },
+    [`@media (max-width: ${mobileSize}px)`]: {
+      width: '100%',
+    },
+  },
+  img: {
+    width: '100%',
+    maxHeight: '230px',
+    [`@media (max-width: ${tabletSize}px)`]: {
+      maxHeight: '246px',
+    },
+    [`@media (max-width: ${mobileSize}px)`]: {
+      maxHeight: '372px',
+    },
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '31px 21px 22px 28px',
+  },
+  title: {
+    margin: '0 0 25px',
+    fontSize: '27px',
+    fontWeight: 500,
+  },
+  '@media (min-width: 767px)': {
+    card: {
+      width: '47.7%',
+    },
+  },
+};
