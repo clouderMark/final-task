099a20433766b30d6779d54f0266d0edc505233d
clouderMark <charnysheuski@yandex.by>
2024-01-22 10:19:11 +0300

    feat: add paginations to all collection view

--- client/src/components/AllCollections.tsx
+++ client/src/components/AllCollections.tsx
@@ -1,5 +1,5 @@
-import {useEffect, useState} from 'react';
-import {Box} from '@mui/material';
+import {ChangeEvent, useEffect, useState} from 'react';
+import {Box, Pagination} from '@mui/material';
 import {useGetAllUserCollectionsMutation} from '../redux/collectionApi';
 import ThemedTypography from './ThemedTypography';
 import ListItem from './ListItem/ListItem';
@@ -8,39 +8,58 @@ import {selectUser} from './LoginUser/redux/userSlice/userSlice';
 import {useAppSelector} from '../redux/hooks';
 import {selectDialogWithTitle} from './DialogWithTitle/dialogWithTitleSlice';
 
-const defaultLimit = 12;
-const defaultPage = 0;
+const defaultLimit = 9;
+const defaultPage = 1;
 
 const AllCollections = () => {
   const {token} = useAppSelector(selectUser);
   const [getData, {data}] = useGetAllUserCollectionsMutation();
-  const [page] = useState(defaultPage);
   const [limit] = useState(defaultLimit);
   const {title} = useAppSelector(selectDialogWithTitle);
+  const [page, setPage] = useState(defaultPage);
+
+  const handlePageChange = (event: ChangeEvent<unknown>, value: number) => {
+    setPage(value);
+  };
 
   useEffect(() => {
     if (token) {
-      getData({page, limit, token});
+      getData({page: page - 1, limit, token});
     }
-  }, [token, title]);
+  }, [token, title, page]);
 
   return (
-    <Box sx={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', mt: 3}}>
+    <Box>
       {data ? (
-        data.collections.map((el) => (
-          <ListItem
-            id={el.id}
-            navigateTo={EPath.Collection}
-            image={el.image}
-            key={el.id}
-            child={
-              <>
-                <ThemedTypography>{el.name}</ThemedTypography>
-                <ThemedTypography>{el.theme}</ThemedTypography>
-              </>
-            }
-          />
-        ))
+        <Box>
+          <Box sx={{display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', mt: 3}}>
+            {data.collections.map((el) => (
+              <ListItem
+                id={el.id}
+                navigateTo={EPath.Collection}
+                image={el.image}
+                key={el.id}
+                child={
+                  <>
+                    <ThemedTypography>{el.name}</ThemedTypography>
+                    <ThemedTypography>{el.theme}</ThemedTypography>
+                  </>
+                }
+              />
+            ))}
+          </Box>
+          {Math.ceil(data.numberOfRecords / limit) > 1 ? (
+            <Box sx={{display: 'flex', justifyContent: 'center'}}>
+              <Pagination
+                count={Math.ceil(data.numberOfRecords / limit)}
+                showFirstButton
+                showLastButton
+                onChange={handlePageChange}
+                page={page}
+              />
+            </Box>
+          ) : null}
+        </Box>
       ) : (
         <ThemedTypography component="p" sx={{mb: 10}}>
           По вашему запросу ничего не найдено
